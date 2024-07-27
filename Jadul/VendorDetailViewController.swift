6526a107496831fea6d549b9cc56a27a386c5023
Fitsyu Two <fitsyu2@gmail.com>
2024-07-27 21:19:19 +0700

    Create empty VendorDetailViewController

---
+++ Jadul/VendorDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  VendorDetailViewController.swift
+//  Jadul
+//
+//  Created by Fitsyu  on 27/07/24.
+//
+
+import UIKit
+
+class VendorDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        self.view.backgroundColor = .white
+    }
+}
