a04f47ac7a6f92af7ebb6021b73a6babbba8f9de
Fitsyu Two <fitsyu2@gmail.com>
2024-07-27 21:19:53 +0700

    Implement basic map functions

--- Jadul/ViewController.swift
+++ Jadul/ViewController.swift
@@ -15,9 +15,14 @@ class ViewController: UIViewController {
     private let locationManager: CLLocationManager = CLLocationManager()
     
     @IBOutlet weak var mapView: MKMapView!
-
+    
+    @IBOutlet weak var loaderView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mapView.register(VendorMarkView.self, forAnnotationViewWithReuseIdentifier: "vendor")
+        mapView.delegate = self
         
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
@@ -31,55 +36,106 @@ class ViewController: UIViewController {
         } else {
             toCenter = CLLocationCoordinate2D(latitude: -6.294143562858167, longitude: 106.78478842425737)
         }
-
-        mapView.setRegion(MKCoordinateRegion(center: toCenter, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)), animated: true)
         
+        self.center(to: toCenter)
         
-        let task = URLSession.shared.dataTask(with: URLRequest(url: URL(string: "https://kmbmicro.co.id/angelhack/vendors")!)) { data, response, error in
+        loaderView.isHidden = false
+        let task = URLSession.shared.dataTask(with: URLRequest(url: URL(string: "https://kmbmicro.co.id/angelhack/vendors")!)) { [weak self] data, response, error in
             
             do {
                 let decoder = JSONDecoder()
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
                 
-                let info = try decoder.decode([Vendor].self, from: data!)
+                let vendors = try decoder.decode([Vendor].self, from: data!)
                 
-                print(info)
-                    
-            } catch {
-                print(error)
+                DispatchQueue.main.async { [weak self] in
+                    self?.handleSuccessfulFetch(vendors: vendors)
+                }
                 
-            }
+            } catch {}
         }
         
         task.resume()
+    }
+    
+    private func handleSuccessfulFetch(vendors: [Vendor]) {
+        vendors.forEach { vendor in
+            
+            let firstLocation = vendor.locationTime.first!
+            let coordinate = CLLocationCoordinate2D(latitude: firstLocation.lat, longitude: firstLocation.lng)
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            annotation.title = vendor.name
+            
+            self.mapView.addAnnotation(annotation)
+            
+            
+            let points = vendor.locationTime.map { CLLocationCoordinate2D(latitude: $0.lat, longitude: $0.lng) }
+            
+            let line = MKPolyline(coordinates: points, count: points.count)
+            self.mapView.addOverlay(line)
+            
+            self.loaderView.isHidden = true
+        }
+    }
+    
+    private func center(to coordinate: CLLocationCoordinate2D) {
+        let delta = 0.05
+        mapView.setRegion(MKCoordinateRegion(center: coordinate, span: MKCoordinateSpan(latitudeDelta: delta, longitudeDelta: delta)), animated: true)
     }
 }
 
 extension ViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else { return }
-     
-        mapView.setRegion(MKCoordinateRegion(center: location.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)), animated: true)
+        self.center(to: location.coordinate)
+        locationManager.stopUpdatingLocation()
     }
 }
 
-struct Vendor: Decodable {
-    let id: Int
-    let name: String
-//    let audio: String
-    
-    let items: [Item]
-    let locationTime: [LocationTime]
-    
-    struct Item: Decodable {
-        let id: Int
-        let name: String
-        let price: Int
+extension ViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: any MKAnnotation) -> MKAnnotationView? {
+        guard !(annotation is MKUserLocation), let title = annotation.title else {
+            return nil
+        }
+        
+        var annotationView: VendorMarkView? = mapView.dequeueReusableAnnotationView(withIdentifier: "vendor") as? VendorMarkView
+        
+        if annotationView == nil {
+            annotationView = VendorMarkView(annotation: annotation, reuseIdentifier: "vendor")
+        } else {
+            annotationView?.annotation = annotation
+        }
+        
+        annotationView?.image = UIImage(named: "vendor")
+        annotationView?.setMessage(msg: title)
+        
+        return annotationView
     }
     
-    struct LocationTime: Decodable {
-        let lat: Double
-        let lng: Double
-        let time: String
+    func mapView(_ mapView: MKMapView, rendererFor overlay: any MKOverlay) -> MKOverlayRenderer {
+        if let polyline = overlay as? MKPolyline {
+            let renderer = MKPolylineRenderer(polyline: polyline)
+            
+            let colors: [UIColor] = [UIColor.systemMint, UIColor.systemCyan, UIColor.systemRed]
+            let index = Int.random(in: 0 ..< colors.count-1)
+            let color = colors[index]
+            
+            renderer.strokeColor = color
+            renderer.lineWidth = 3.0
+            return renderer
+        }
+        
+        return MKOverlayRenderer()
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        
+        if view.annotation is MKUserLocation { return }
+        
+        let dvc = VendorDetailViewController()
+        self.present(dvc, animated: true)
     }
 }
+
