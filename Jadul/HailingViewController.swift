9a37847caf66cedab267e4abe2d47081941d91d3
Fitsyu <fitrahsyuhada@rocketmail.com>
2024-07-27 23:53:58 +0700

    Implement Hailing

--- Jadul/HailingViewController.swift
+++ Jadul/HailingViewController.swift
@@ -5,13 +5,29 @@
 //  Created by Fitsyu  on 27/07/24.
 //
 
+import Lottie
 import UIKit
 
 class HailingViewController: UIViewController {
     
-    @IBOutlet weak var hailingView: UIView!
+    @IBOutlet weak var hailerView: UIView!
+    
+    var lottieView: LottieAnimationView?
+
     
     @IBAction func stopTapped() {
         dismiss(animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        lottieView = .init(name: "hail")
+        lottieView?.contentMode = .scaleAspectFit
+        lottieView?.loopMode = .loop
+        lottieView?.frame = hailerView.bounds
+        lottieView?.play()
+        
+        self.hailerView.addSubview(lottieView!)
     }
 }
