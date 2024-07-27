729671c4d65fa9c7ee7de8a5d451c2528ff58303
Fitsyu Two <fitsyu2@gmail.com>
2024-07-27 21:16:58 +0700

    Create Vendor entity

---
+++ Jadul/Vendor.swift
@@ -0,0 +1,29 @@
+//
+//  Vendor.swift
+//  Jadul
+//
+//  Created by Fitsyu  on 27/07/24.
+//
+
+import Foundation
+
+struct Vendor: Decodable {
+    let id: Int
+    let name: String
+    let audio: URL
+    
+    let items: [Item]
+    let locationTime: [LocationTime]
+    
+    struct Item: Decodable {
+        let id: Int
+        let name: String
+        let price: Int
+    }
+    
+    struct LocationTime: Decodable {
+        let lat: Double
+        let lng: Double
+        let time: String
+    }
+}
